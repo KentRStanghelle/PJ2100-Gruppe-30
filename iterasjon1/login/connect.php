0db5346e52056da40e38c88ac8864298bfd171dc
wiujo14 <jowium92@hotmail.com>
2015-03-13 13:01:03 +0100

    endret passord til database i connect.php

--- iterasjon1/login/connect.php
+++ iterasjon1/login/connect.php
@@ -2,7 +2,7 @@
     function dbConnect(){
         try{
             $username = 'root';
-            $password = 'root';
+            $password = '';
             $conn = new pdo("mysql:host=localhost;dbname=users;", $username, $password);
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             return $conn;
